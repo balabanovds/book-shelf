90393d1b0df8dda381308613dc336155f84a7093
Denis <balabanov.ds@gmail.com>
2020-08-07 15:54:38 +0300

    fix moment locales big size issue

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+const webpack = require('webpack')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/book-shelf/'
-    : '/'
+    : '/',
+  configureWebpack: {
+    plugins: [
+      new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /ru|en/)
+    ]
+  }
 }
